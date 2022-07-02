bdc9ed9559e361dc1e62f384f243c4de24e2ffee
kaiuri <uriel.acioli@gmail.com>
2022-07-02 11:16:19 -0400

    more help hl-groups

--- lua/nvim-mariana/init.lua
+++ lua/nvim-mariana/init.lua
@@ -218,7 +218,7 @@ local highlights = {
 
     -- HELP
 
-    helpTSTitle = { fg = '#EF7A5D', bold = true },
+    helpTSTitle = { fg = '#EF7A5D' },
     helpTSTextReference = { link = 'TSURI' },
 
     -- DIAGNOSTICS
