988deee52f4e0d6127203e3aa6630ed080fdfccb
kaiuri <19148108+kaiuri@users.noreply.github.com>
2022-08-26 10:23:19 -0400

    use palette for lualine colors

--- lua/lualine/themes/juliana.lua
+++ lua/lualine/themes/juliana.lua
@@ -1,43 +1,60 @@
-local active = '#3d4752'
-local inactive = '#788797'
-local bg = '#2B333B'
-local fg = '#cbd3e2'
-local black = '#252b32'
-local red = '#ec5f66'
-local green = '#99c794'
-local yellow = '#fac761'
-local cyan = '#5fb4b4'
+-- use original juliana colors
+local ok, juliana = pcall(require, 'nvim-juliana.palette')
+local inactive
+local bg
+local fg
+local red
+local green
+local yellow
+local cyan
+if not ok then
+  inactive = '#788797'
+  bg = '#2b333b'
+  fg = '#cbd3e2'
+  red = '#ec5f66'
+  green = '#99c794'
+  yellow = '#fac761'
+  cyan = '#5fb4b4'
+else
+  inactive = juliana.blue6
+  bg = '#2b333b'
+  fg = juliana.white3
+  red = juliana.red
+  green = juliana.green
+  yellow = juliana.orange
+  cyan = juliana.blue5
+end
 
 return {
-    normal = {
-        a = { bg = bg, fg = fg, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
-    insert = {
-        a = { bg = bg, fg = green, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
-    visual = {
-        a = { bg = bg, fg = cyan, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
-    replace = {
-        a = { bg = bg, fg = red, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
-    command = {
-        a = { bg = bg, fg = yellow, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
-    inactive = {
-        a = { bg = bg, fg = inactive, gui = 'bold' },
-        b = { bg = bg, fg = fg },
-        c = { bg = bg, fg = fg },
-    },
+  normal = {
+    a = { bg = bg, fg = fg, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
+  insert = {
+    a = { bg = bg, fg = green, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
+  visual = {
+    a = { bg = bg, fg = cyan, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
+  replace = {
+    a = { bg = bg, fg = red, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
+  command = {
+    a = { bg = bg, fg = yellow, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
+  inactive = {
+    a = { bg = bg, fg = inactive, gui = 'bold' },
+    b = { bg = bg, fg = fg },
+    c = { bg = bg, fg = fg },
+  },
 }
 -- require('lualine').setup {options = {theme = mariana}}
