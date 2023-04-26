6783c43ed9b5c85fa89d4cb25978ee9178611063
kaiuri <uriel.acioli@gmail.com>
2023-04-26 10:16:52 -0300

    perf(init.lua): remove unnecessary code

--- lua/nvim-juliana/init.lua
+++ lua/nvim-juliana/init.lua
@@ -1,79 +1,41 @@
 local theme = require('nvim-juliana.theme')
 
---- Default configuration
----@class Configuration
-local DEFAULT = {
-  ---@class Colors
-  colors = {
-    -- suggested Visual highlight #4b5864
-    bg1 = '#3b454e',
-    bg2 = '#303841',
-    bg3 = '#2e353e',
-    fg1 = '#ffffff',
-    fg2 = '#d8dee9',
-    fg3 = '#a6acb8',
-    fg4 = '#46525c',
-    shade1 = '#2f373f',
-    shade2 = '#2e363e',
-    blue1 = '#95b2d6',
-    blue2 = '#5c99d6',
-    cyan = '#5fb4b4',
-    green = '#99c794',
-    magenta = '#cc8ec6',
-    orange = '#f97b58',
-    red = '#ec5f66',
-    yellow1 = '#fac761',
-    yellow2 = '#f9ae58',
-    yellow3 = '#ee932b',
-  },
-}
+local DEFAULT = require 'nvim-juliana.defaults'
 
---- Main module
 ---@class Juliana
-local M = setmetatable({
-  config = DEFAULT,
-}, {})
+local M = setmetatable({ config = DEFAULT }, {})
 
 --- Takes a table conforming to the Configuration interface and merges it with
 -- the default configuration
 ---@param config? Configuration
-function M:setup(config)
+M.setup = function(config)
   if not config then return end
-  self.config = vim.tbl_deep_extend('force', DEFAULT, config)
+  M.config = vim.tbl_deep_extend('force', DEFAULT, config)
 end
 
 --- Loads the theme
----@param func fun(theme: Colors): {[string]: table<string, any>?} # Function that returns a table of highlight<br> groups and their respective definitions
+---@param func? fun(theme: Colors): {[string]: table<string, any>?} # Function that returns a table of highlight<br> groups and their respective definitions
 ---@return nil
 function M:load(func)
+  func = func or theme
+
   if vim.g.colors_name then
     vim.cmd('hi clear')
   end
   vim.cmd('set t_Co=256')
-  vim.g.colors_name = 'juliana'
+  vim.g.colors_name = 'kaiurin'
 
   local colorscheme = func(self.config.colors)
   local nvim_set_hl = vim.api.nvim_set_hl
-  for group_name, definition in pairs(colorscheme) do
-    nvim_set_hl(0, group_name, definition)
+
+  for _, v in ipairs(colorscheme) do
+    nvim_set_hl(0, v[1], v[2])
   end
 end
 
-function M:get_colors()
-  return self.config.colors
+---@return Colors
+M.colors = function()
+  return M.config.colors
 end
 
-return {
-  --- loads the colorscheme
-  ---@return nil
-  load = function()
-    M:load(theme)
-  end,
-  colors = function()
-    return M:get_colors()
-  end,
-  ---@param config Configuration
-  setup = function(config)
-    M:setup(config)
-  end,
-}
+return M
