93b0393844645408371a0c5710ab2cef472ca042
kaiuri <uriel.acioli@gmail.com>
2023-08-06 08:39:09 -0300

    refactor(nvim-juliana/init.lua): simplify code

--- lua/nvim-juliana/init.lua
+++ lua/nvim-juliana/init.lua
@@ -1,6 +1,3 @@
-local DEFAULT = require 'nvim-juliana.defaults'
-local theme = require('nvim-juliana.theme')
-
 local nvim_set_hl = vim.api.nvim_set_hl
 ---@param group string
 ---@param style table<string, any>
@@ -8,17 +5,25 @@ local highlight = function(group, style)
   nvim_set_hl(0, group, style)
 end
 
+---Missing configuration fields will be filled with the default values
 ---@class Juliana
-local M = setmetatable({ config = DEFAULT }, {})
+local M = {
+  ---@class Configuration
+  config = {
+    colors = require 'nvim-juliana.defaults',
+  },
+}
 
 ---Takes a table conforming to the Configuration interface,
 ---merges it with the default configuration
 ---@param config? Configuration
 M.setup = function(config)
   if not config then return end
-  M.config = vim.tbl_deep_extend('force', DEFAULT, config)
+  M.config = vim.tbl_deep_extend('force', M.config, config)
 end
 
+
+local theme = require('nvim-juliana.theme')
 --- Loads the theme
 ---@return nil
 function M:load()
@@ -27,7 +32,7 @@ function M:load()
   end
   vim.cmd('set t_Co=256')
   vim.g.colors_name = 'juliana'
-  local colorscheme = theme(self.config.colors, highlight)
+  theme(self.config.colors, highlight)
 end
 
 ---@return Colors
