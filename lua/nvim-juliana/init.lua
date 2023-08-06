0c8ee381027e3026eabcb38dbbf24f36c7d88f92
Uriel Acioli <uriel.acioli@gmail.com>
2023-08-06 08:02:29 -0400

    fixes and improvements (#24)
    * doc(init.lua/setup): remove unnecessary comments
    
    * perf: remove unnecessary loop during theme loading
    
    `require('nvim-juliana'):load` now takes no argument and theme takes
    two. That way we can load the theme in one pass and keep things separated.
    
    It also enables future extension by overloading the highlighting
    function (WIP)
    
    * refactor(nvim-juliana/theme): use link instead of sharing the same variable
    
    * refactor(nvim-juliana/theme): use link instead of sharing the same variable
    
    * doc(nvim-juliana/theme.lua): improve docstring
    
    * refactor(nvim-juliana/defaults.lua): remove unnecessary table nesting
    
    * refactor(nvim-juliana/init.lua): simplify code
    
    * feat: add statusline highlighting groups
    
    fix: link hl-WinBar with hl-StatusLine
    
    * fix(nvim-juliana/theme.lua): more descriptive theme function declaration

--- lua/nvim-juliana/init.lua
+++ lua/nvim-juliana/init.lua
@@ -1,36 +1,38 @@
-local theme = require('nvim-juliana.theme')
+local nvim_set_hl = vim.api.nvim_set_hl
+---@param group string
+---@param style table<string, any>
+local highlight = function(group, style)
+  nvim_set_hl(0, group, style)
+end
 
-local DEFAULT = require 'nvim-juliana.defaults'
-
+---Missing configuration fields will be filled with the default values
 ---@class Juliana
-local M = setmetatable({ config = DEFAULT }, {})
+local M = {
+  ---@class Configuration
+  config = {
+    colors = require 'nvim-juliana.defaults',
+  },
+}
 
---- Takes a table conforming to the Configuration interface and merges it with
--- the default configuration
+---Takes a table conforming to the Configuration interface,
+---merges it with the default configuration
 ---@param config? Configuration
 M.setup = function(config)
   if not config then return end
-  M.config = vim.tbl_deep_extend('force', DEFAULT, config)
+  M.config = vim.tbl_deep_extend('force', M.config, config)
 end
 
---- Loads the theme
----@param func? fun(theme: Colors): {[string]: table<string, any>?} # Function that returns a table of highlight<br> groups and their respective definitions
----@return nil
-function M:load(func)
-  func = func or theme
 
+local theme = require('nvim-juliana.theme')
+--- Loads the theme
+---@return nil
+function M:load()
   if vim.g.colors_name then
     vim.cmd('hi clear')
   end
   vim.cmd('set t_Co=256')
   vim.g.colors_name = 'juliana'
-
-  local colorscheme = func(self.config.colors)
-  local nvim_set_hl = vim.api.nvim_set_hl
-
-  for _, v in ipairs(colorscheme) do
-    nvim_set_hl(0, v[1], v[2])
-  end
+  theme(self.config.colors, highlight)
 end
 
 ---@return Colors
