6a06227a968ecd1e2c7c7d243f1c56fc955007b4
kaiuri <uriel.acioli@gmail.com>
2023-08-06 08:54:35 -0300

    fix(nvim-juliana/theme.lua): more descriptive theme function declaration

--- lua/nvim-juliana/theme.lua
+++ lua/nvim-juliana/theme.lua
@@ -4,7 +4,7 @@
 ---@param colors Colors # palette of colors
 ---@param highlight fun(group: string, style: table<string, any>): nil # highlighting function
 return function(colors, highlight)
-  local C = colors
+  local C                   = colors
   local default             = { fg = C.fg2, bg = C.bg2 }
   local muted               = { fg = C.fg3 }
   local selection           = { bg = C.selection_bg }
